3d87df7a55656a367aa0a3f287a2348900df0c3e
MMKZA <86630716+MMKZA@users.noreply.github.com>
2022-07-05 21:18:57 +0700

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -325,9 +325,9 @@ async def youtube_dl_call_back(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "© @xTeamBots"
+                caption = "@Film_Day_Bot"
                 if is_w_f:
-                    caption = "@xurluploaderbot"
+                    caption = "@Film_Day_Bot"
                 for image in images:
                     if os.path.exists(str(image)):
                         if i == 0:
